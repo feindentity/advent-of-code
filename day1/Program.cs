1e9f3f38140d3d31306d0c5001327e59276cd0ba
feindentity <81498334+feindentity@users.noreply.github.com>
2021-12-18 16:47:43 +0000

    Initial Code to process input

---
+++ day1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace day1
+{
+    class Program
+    {
+        static string[] LoadInputArray(string fileName)
+        {
+            string[] inputList;
+            inputList=System.IO.File.ReadAllLines(fileName);
+            return inputList;
+        }
+        static void Main(string[] args)
+        {
+            string[] currentInputList;
+            currentInputList=LoadInputArray("input.txt");
+            int increaseCount=0;
+            int prevValue = int.Parse(currentInputList[0]);
+            int currentValue;         
+            for(int counter=1; counter < currentInputList.Length; counter++)
+            {
+                currentValue = int.Parse(currentInputList[counter]);
+                if (currentValue > prevValue)
+                {
+                    increaseCount++;
+                }
+                prevValue=currentValue;
+            }
+            Console.WriteLine(increaseCount);
+        }
+    }
+}
