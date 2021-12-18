8f619799d129d49dd8dca636e4edc7b4e632dcdd
feindentity <81498334+feindentity@users.noreply.github.com>
2021-12-18 19:28:07 +0000

    Update with Code for Part 2

--- day1/Program.cs
+++ day1/Program.cs
@@ -34,18 +34,38 @@ namespace day1
         static int[] tripledSummedList(int[] initialInputList)
         {
             int[] summedList=new int[1998];
+            int firstValue = initialInputList[0];
+            int secondValue = initialInputList[1];
+            int thirdValue;
+            int tripledCounter=0;
+            for(int counter=2;counter< initialInputList.Length;counter++)
+            {
+                thirdValue=initialInputList[counter];
+                summedList[tripledCounter]=firstValue+secondValue+thirdValue;
+                tripledCounter++;
+                firstValue=secondValue;
+                secondValue=thirdValue;
+            }
             return(summedList);
         }
         static void Main(string[] args)
-        {
-           Part1();
-        }
-        static void Part1()
-        {
-             int[] currentInputList;
+        {  int[] currentInputList;
             currentInputList=LoadInputArray("input.txt");
+           Part1(currentInputList);
+           Part2(currentInputList);
+        }
+        static void Part1(int[] currentInputList)
+        {
+           
             int increaseCount=DetermineIncreaseCount(currentInputList);
             Console.WriteLine("Part 1 Individual Increases: {0}",increaseCount);
         }
+        static void Part2(int[] currentInputList)
+        {
+            int[] summedArray=tripledSummedList(currentInputList);
+            int increaseCount=DetermineIncreaseCount(summedArray);
+            Console.WriteLine("Part 2 Tripled Sum Increases: {0}",increaseCount);
+        }
+
     }
 }
