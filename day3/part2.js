52ab56bc98c3268bc7df4b290961df9dc0ca8b3c
feindentity <81498334+feindentity@users.noreply.github.com>
2021-12-19 03:50:21 +0000

    sample code for node

--- day3/part2.js
+++ day3/part2.js
@@ -1,3 +1,12 @@
 var fs = require('fs');
 var readline=require('readline')
-console.log("test");
+var binaryString = "1010";
+var testNumber = parseInt(binaryString,2);
+var testCompare = (binaryString.charAt(0)=='1');
+var testGenerateGamma = '1';
+testGenerateGamma = testGenerateGamma + '2';
+console.log(testNumber);
+console.log(binaryString.charAt(0));
+console.log(binaryString.charAt(1));
+console.log(testCompare);
+console.log(testGenerateGamma);
