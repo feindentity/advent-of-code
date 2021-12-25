7a6138f67bbbc145eafd05baf45909f528fd8291
feindentity <81498334+feindentity@users.noreply.github.com>
2021-12-25 18:30:07 +0000

    Intial break up but try new formula

--- day6/part1.js
+++ day6/part1.js
@@ -23,24 +23,65 @@ var AddFish=function(currentFishList,numberOfFishToAdd)
     }
 }
 
-var fishList = GetFish("input.txt");
-console.log(fishList.length);
-var maxGeneration = 80;
-//var maxGeneration=256;
-for(var dayCounter=0;dayCounter<maxGeneration;dayCounter++)
-{   var newFishCounter=0;
-    for(var fishCounter=0;fishCounter<fishList.length;fishCounter++)
-    {
-        if (fishList[fishCounter]==0)
-        {
-            fishList[fishCounter]=6;
-            newFishCounter++;
-        }
-        else
-        {
-            fishList[fishCounter]--;
-        }
+var SchoolGeneration=function(schoolList, maxGeneration)
+{
+    var schoolDistrict=[schoolList]
+    for(var dayCounter=0;dayCounter<maxGeneration;dayCounter++)
+    {   var newFishCounter=0;
+            for(var districtCounter=0; districtCounter<schoolDistrict.length;districtCounter++)
+            {
+                for(var fishCounter=0;fishCounter<schoolDistrict[districtCounter].length;fishCounter++)
+                {
+        
+                    if (schoolDistrict[districtCounter][fishCounter]==0)
+                    {
+                        schoolDistrict[districtCounter][fishCounter]=6;
+                        newFishCounter++;
+                    }
+                    else
+                    {
+                        schoolDistrict[districtCounter][fishCounter]--;
+                    }
+                }
+            }
+       var SplitCounter=newFishCounter;
+       
+       while(SplitCounter>0)
+       {
+            if (SplitCounter <  1000000)
+            {
+            var generationSchool = [];
+            AddFish(generationSchool,SplitCounter);
+            SplitCounter=0;
+            }
+            else
+            {
+            var generationSchool = [];
+            AddFish(generationSchool,1000000);
+            SplitCounter=SplitCounter-1000000;
+            }
+            schoolDistrict.push(generationSchool);      
+       }
+//            var generationSchool = [];
+//            AddFish(generationSchool,newFishCounter);
+        console.log("day: " + dayCounter);
+        console.log("genneration number: " +  schoolDistrict.length);
     }
-    AddFish(fishList,newFishCounter);
+    return schoolDistrict;
 }
-console.log("Amount of Fish: " + fishList.length);
+var fishList = GetFish("input.txt");
+var maxGeneration = 256;
+var school1 = fishList.slice(0,1);
+var currentSchoolDistrict = SchoolGeneration(school1,maxGeneration);
+console.log("Generation Count: " + currentSchoolDistrict.length);
+var fishCounter=0;
+for (var districtCounter=0; districtCounter < currentSchoolDistrict.length;districtCounter++)
+{
+    fishCounter+=currentSchoolDistrict[districtCounter].length;
+}
+//var maxGeneration=256;
+console.log("Fish Count: " + fishCounter);
+//console.log("Amount of Fish: " + schoolListCounter1);
+
+//calculate a new fish for a number generates
+var fish=[8];
